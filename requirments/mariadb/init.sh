5d1cb5fecc35d5bbc0221083e442f8fd21bc4b2d
Suzuka Maei <smaei@c1r1s5.42tokyo.jp>
2024-12-12 21:41:28 +0900

    inception

---
+++ requirments/mariadb/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# confの書き換え
+echo "change bind-address..."
+sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+
+# mariadbのデータディレクトリが存在しない場合、初期化を行う
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "Initializing MariaDB data directory..."
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
+else
+  echo "MariaDB data directory already initialized. Skipping initialization."
+fi
+
+# MariaDBをバックグラウンドで実行
+mysqld_safe &
+
+# MariaDBが起動するまで待機
+until mysqladmin ping -h localhost --silent; do
+  echo "Waiting for MariaDB to be ready..."
+  sleep 2
+done
+
+# データベースの作成
+echo "Running SQL script..."
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" > build.sql
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" >> build.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> build.sql
+echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';" >> build.sql
+echo "FLUSH PRIVILEGES ;" >> build.sql
+
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} < build.sql
+
+# #MariaDB プロセスを前面で実行し続ける
+wait
+# exec "$@"
---
+++ requirments/wordpress/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd /var/www/html
+if [ ! -f /var/www/html/index.php ]; then
+    wp core download --allow-root
+else
+    echo "WordPress already installed"
+fi
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+else
+    echo "wp-config.php already exists. Skipping config creation."
+fi
+
+if ! wp core is-installed --allow-root; then
+    wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+    wp user create --url=$WORDPRESS_URL $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=$WORDPRESS_USER_ROLE --allow-root
+
+else
+    echo "WordPress is already installed. Skipping installation."
+fi
+
+exec php-fpm7.4 -F
+_m4ZGt6_
